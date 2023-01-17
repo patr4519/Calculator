fac8facc58ecd26fd354e6c6b7ec1688f0b742f8
patr4519 <jero96@mail.ru>
2023-01-17 14:44:06 +0300

    +

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -4,14 +4,27 @@ import { Inputs } from "./Inputs";
 
 export const Calculator = () => {
     const [currentValue, setCurrentValue] = React.useState(0);
-    const [inputedValue, setInputedValue] = React.useState('');
+    const [inputedArr, setInputedArr] = React.useState([]);
+
+    const calculate = () => {
+        setCurrentValue(eval(inputedArr.join('')))
+    }
+
+    const allClear = () => {
+        setCurrentValue(0);
+        setInputedArr([]);
+    }
 
     return (
         <div className="calculator">
             <Results
-            currentValue={currentValue}
-            inputedValue={inputedValue} />
-            <Inputs />
+                currentValue={currentValue}
+                inputedValue={inputedArr} />
+
+            <Inputs
+                setInputedArr={setInputedArr}
+                calculate={calculate}
+                allClear={allClear} />
         </div>
     );
 }
--- src/Components/Inputs.jsx
+++ src/Components/Inputs.jsx
@@ -1,36 +1,37 @@
 import React from "react";
 
-export const Inputs = () => {
+export const Inputs = ({ setInputedArr, calculate, allClear }) => {
+
     return (
         <div className="inputs">
             <div className="row1">
-                <button className="button_calc">AC</button>
+                <button className="button_calc" onClick={allClear}>AC</button>
                 <button className="button_calc">Del</button>
-                <button className="button_calc">%</button>
-                <button className="button_calc">/</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '%'])}>%</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '/'])}>/</button>
             </div>
             <div className="row2">
-                <button className="button_calc">7</button>
-                <button className="button_calc">8</button>
-                <button className="button_calc">9</button>
-                <button className="button_calc">*</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '7'])}>7</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '8'])}>8</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '9'])}>9</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '*'])}>*</button>
             </div>
             <div className="row3">
-                <button className="button_calc">4</button>
-                <button className="button_calc">5</button>
-                <button className="button_calc">6</button>
-                <button className="button_calc">-</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '4'])}>4</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '5'])}>5</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '6'])}>6</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '-'])}>-</button>
             </div>
             <div className="row4">
-                <button className="button_calc">1</button>
-                <button className="button_calc">2</button>
-                <button className="button_calc">3</button>
-                <button className="button_calc">+</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '1'])}>1</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '2'])}>2</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '3'])}>3</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '+'])}>+</button>
             </div>
             <div className="row5">
-                <button className="button_calc zero">0</button>
-                <button className="button_calc">.</button>
-                <button className="button_calc">=</button>
+                <button className="button_calc zero" onClick={() => setInputedArr((prev) => [...prev, '0'])}>0</button>
+                <button className="button_calc" onClick={() => setInputedArr((prev) => [...prev, '.'])}>.</button>
+                <button className="button_calc" onClick={calculate}>=</button>
             </div>
         </div>
     );
