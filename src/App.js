3717c1fc78f780fd126d3e1c65c03b5f4ce2444c
patr4519 <jero96@mail.ru>
2023-01-18 22:44:48 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Calculator } from './Components/Calculator';
+import { Header } from './Components/Header';
+import { Footer } from './Components/Footer';
 
 function App() {
   return (
+    <>
+      <Header />
       <Calculator />
+      <Footer />
+    </>
   );
 }
 
